365b77c7d4f0ef418991b16f0fb69e14ec962d68
Jaeyeop <woduq1204@gmail.com>
2020-10-09 20:42:23 +0900

    Json data binding

---
+++ src/main/java/com/spring/demo/Driver.java
@@ -0,0 +1,38 @@
+package com.spring.demo;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+
+import java.io.File;
+import java.io.IOException;
+
+public class Driver {
+
+    public static void main(String[] args) {
+
+        try {
+
+            // create object mapper
+            ObjectMapper objectMapper = new ObjectMapper();
+
+            // read JSON file and map/convert to Java POJO: data/sample-lite.json
+            Student student = objectMapper.readValue(new File("data/sample-full.json"), Student.class);
+
+            // print first name and last name
+            System.out.println("student.getFirstName() = " + student.getFirstName());
+            System.out.println("student.getLastName() = " + student.getLastName());
+
+            // print out address: city
+            System.out.println("student.getAddress().getCity() = " + student.getAddress().getCity());
+
+            // print out languages
+            for (String language : student.getLanguages()) {
+                System.out.println("language = " + language);
+            }
+
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+    }
+
+}
---
+++ src/main/java/com/spring/demo/Student.java
@@ -0,0 +1,17 @@
+package com.spring.demo;
+
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+import lombok.Data;
+
+@JsonIgnoreProperties(ignoreUnknown = true)
+@Data
+public class Student {
+
+    private Long id;
+    private String firstName;
+    private String lastName;
+    private boolean active;
+    private Address address;
+    private String[] languages;
+
+}
